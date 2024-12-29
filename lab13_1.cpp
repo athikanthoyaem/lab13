8e11931fa0af53de2229de1b594d8269af318942
Athikan <athikan_thoyaem@cmu.ac.th>
2024-12-29 22:26:28 +0700

    lab13_1 complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -18,4 +18,31 @@ int main(){
     cout << "Max = " << B[4] << endl;
     cout << "Min = " << B[5];
     return 0;
+
 }
+
+void stat(const double arr[], int n, double result[]) {
+    double sum = 0.0;
+    double sum_sq = 0.0;
+    double harmonic_sum = 0.0;
+    double product = 1.0;
+    double max_val = arr[0];
+    double min_val = arr[0];
+
+    for (int i = 0; i < n; ++i) {
+        sum += arr[i];
+        sum_sq += arr[i] * arr[i];
+        harmonic_sum += 1.0 / arr[i];
+        product *= arr[i];
+        if (arr[i] > max_val) max_val = arr[i];
+        if (arr[i] < min_val) min_val = arr[i];
+    }
+
+    result[0] = sum / n;
+    result[1] = sqrt((sum_sq / n) - (result[0] * result[0]));
+    result[2] = pow(product, 1.0 / n);
+    result[3] = n / harmonic_sum;
+    result[4] = max_val;
+    result[5] = min_val;
+}
+
